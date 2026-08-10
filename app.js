647ab37e51732dc8094b3b2adc6247605fc818b7
Frank Otoo <franknaro817@gmail.com>
2026-08-10 17:08:01 +0000

    Update app.js

--- app.js
+++ app.js
@@ -161,7 +161,7 @@ const DESTINATIONS = [
     categoryName: "Sacred Sanctuary",
     region: "Upper East Region",
     location: "Paga, Burkina Faso Border",
-    image: "assests/images/Paga.jpg",
+    image: "assets/images/Paga.jpg",
     rating: 4.6,
     reviews: 580,
     fee: "GHS 50 / $4",
